a9c1369f6f72d6bb19a707346701ddb6a2928e51
João <joaopaulo.allex@gmail.com>
2022-03-21 13:35:58 -0300

    Exemplo de procedimento com java

---
+++ src/TesteFuncao.java
@@ -0,0 +1,14 @@
+
+public class TesteFuncao {
+
+	static void soma (int a, int b){
+		int s = a + b;
+		System.out.println("A soma é " + s);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("O programa começou");
+		soma(5,2);
+	}
+
+}
